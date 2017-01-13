8cf73dc8c3554aa9b2ed9c59b5447d23e9ec618b
repository4poc <varinder_gupta82@yahoo.com>
2017-01-13 17:59:35 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,8 +2,5 @@ import org.example
 public class Test
 {
 public static void main(String args[]){
- System.out.println("hello varinder");
- System.out.println("hello varinder12");
- System.out.println("hello varinder 1234567");
 }
 }
