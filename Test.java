f5dcdd2349c47f91e9f3c754f43cf8a14d208141
repository4poc <varinder_gupta82@yahoo.com>
2017-01-13 18:08:50 +0530

    update
    update

--- Test.java
+++ Test.java
@@ -2,10 +2,5 @@ import org.example
 public class Test
 {
 public static void main(String args[]){
- System.out.println("hello varinder");
- System.out.println("hello varinder12");
- System.out.println("Jenkins_Jira JJ-1 testing commit");
- System.out.println("Jenkins_Jira JJ-1 testing commit....");
-  System.out.println("Jenkins_Jira JJ-1 testing");
 }
 }
